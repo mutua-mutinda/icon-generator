8101e76db8bf457fd1cbcbebf6b9b0f34f3fbc64
mutua-mutinda <mutuamutinda@outlook.com>
2021-09-30 15:33:21 +0300

    added mirror func

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,11 +3,23 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    hex
+    |> Enum.chunk_every(3)
+    |> mirror_row
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+    row ++ [second, first]
   end
 
   def pick_color(image) do
     %Identicon.Image{hex: [r, g, b | _tail]} = image
-    [r, g, b]
+    %Identicon.Image{image | color: {r, g, b}}
   end
 
   def hash_input(input) do
